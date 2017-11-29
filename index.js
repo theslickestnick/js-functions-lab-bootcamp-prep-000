0b7b02c277689beb34ae4e08ffb2e7943dc4cf22
Nicholas Echevarria <nick.echev@gmail.com>
2017-11-29 02:48:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-function happyHolidaysTo(holiday, name) {
+function happyHolidaysTo(name) {
   return ("Happy holidays, " + name + "!")
 }
 function happyHolidayTo(holiday, name) {
